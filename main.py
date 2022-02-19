66d7ff5d05a626c4a0688d8f3c113588d62540e1
Kunal Pawar <72133375+KunalPawar0207@users.noreply.github.com>
2022-02-19 15:46:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,14 +10,13 @@ from sklearn.metrics import accuracy_score
 from PIL import Image
 import PIL.ImageOps
 
-x,y = fetch_openml("mnist_784",version = 1, return_X_y= True)
 x = np.load("image.npz")["arr_0"]
 y = pd.read_csv("labels.csv")["labels"]
 # print(pd.Series(y).value_counts())
 classes = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K','L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 nclasses = len(classes)
 
-x_train,x_test,y_train,y_test = train_test_split(x,y, random_state = 0,train_size=7500,test_size = 2500)
+x_train,x_test,y_train,y_test = train_test_split(x,y, random_state = 0,train_size=3500,test_size = 500)
 xtrainscaled = x_train/255.0
 xtestscaled = x_test/255.0
 
@@ -49,7 +48,7 @@ while(True):
         # L it means - each pixel is represented as 0 to 255
         # antialias - it is use to minimize the distortion of the image
         img_bw = im_pil.convert("L")
-        img_bw_resized = img_bw.resized((28,28),Image.ANTIALIAS)
+        img_bw_resized = img_bw.resize((28,28),Image.ANTIALIAS)
         img_bw_resized_inverted = PIL.ImageOps.invert(img_bw_resized)
         pixel_filter = 20
         min_pixel = np.percentile(img_bw_resized_inverted,pixel_filter)
